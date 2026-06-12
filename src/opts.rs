062ba156477ee56cf3ca7247bde9c2ef324f3c4d
Realm1e <koiymira991@gmail.com>
2026-06-13 01:51:04 +0900

    Update opts.rs

--- src/opts.rs
+++ src/opts.rs
@@ -2,5 +2,5 @@ pub const BACKEND_URL: &str = "http://209.25.142.22:1154"; // Set valid IP, not 
 
 // Partyhub shit
 pub const USE_PARTYHUB: bool = true;
-pub const LOGIN_URL: &str = "http://209.25.142.22:1154/login";
-pub const REGISTER_URL: &str = "http://209.25.142.22:1154/register";
+pub const LOGIN_URL: &str = "http://209.25.142.22:1068/register";
+pub const REGISTER_URL: &str = "http://209.25.142.22:1068/register";
